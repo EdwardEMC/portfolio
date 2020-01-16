c3cff7bfee681d541e5730b2cdde71f61a4d834f
EMC <edwardcoad.ec@gmail.com>
2020-01-16 19:26:46 +1030

    updated picture links

--- assets/js/information.js
+++ assets/js/information.js
@@ -1,7 +1,7 @@
 const images = [ //information to do with the portfolio
     [{      
             title: "Calendar",
-            url: "assets/images/calendar.jpg", 
+            url: "assets/images/Calendar.jpg", 
             href: "https://edwardemc.github.io/calendar/",
             repo: "https://github.com/EdwardEMC/calendar",
             text: "A calendar application to schedule events. The entire application is dynamically updated with moment.js so that the days of the month are always in the correct position and the date is correct. Hovering over the days will show the user which day is being focused and clicking on any one of the days will bring up the day planner where information about a certain day/time can be saved. All days in the past and present are displayed as simple input boxes while the current date (red color coded date display) will bring up the advanced day planner (updated hourly to keep track of events). It works by placing text within the coloured boxes and then saving them with the save button provided. It is colour coded to represent the present hour in red, any previous hour slots in grey and any hour slots in the future in green. On load the application checks the local storage for the date and any previously save data. Any days with data saved will be displayed with a light green background color on the main calendar page as to signal to the user there is something within this day. There are two clear buttons one on the main calendar page and one inside every day planner extension, clear simply empties the html elements local to the page while clear all will empty the elements and the local storage for all days."
@@ -15,7 +15,7 @@ const images = [ //information to do with the portfolio
         }, 
         {
             title: "Code Quiz",
-            url: "assets/images/triviaGame.jpg", 
+            url: "assets/images/TriviaGame.jpg", 
             href: "https://edwardemc.github.io/code-Quiz/",
             repo: "https://github.com/EdwardEMC/code-Quiz",
             text: "A series of quizzes to test your knowledge on the various topics. The quiz begins on the title page where you can select one of three topics, javaScript, html or css. Pressing one of these will direct you to the ready screen; it's simply a button to start the quiz and a basic definition of what the topic is below. Each quiz is timed depending on the number of questions, to keep everything uniform each quiz has 6 different questions but more can be added if necessary. To select an answer hover over it with your mouse, it should be highlighted in blue and click it. An alert will let the user know if they have been sucessful or not, if not a certain amount of time will be deducted from their overal time remaining. At the end of the questions, the user will be prompted to input their initials after which their score and initials will be saved to the local storage and displayed on the highscore page in order of score (highest to lowest). If the user wishes to go again or choose a different quiz they just have to click the home link in the navigation bar."
