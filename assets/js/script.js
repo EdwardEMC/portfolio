876aaead1dd84887a4f2d164fce5aaf190a35818
EMC <edwardcoad.ec@gmail.com>
2020-01-16 18:11:03 +1030

    updated portfolio imported from local file - updatedPortfolio on github

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,172 +1,159 @@
-var planner = $(".day-planner");
-var hour = moment().format('HH');
+window.onload = function() {
+    location.href = "#home";
+};
 
-//Array for time values, to increase the day planner simply add the new time to the array and everything will dynamically update
-var time = ["9:00am", "10:00am", "11:00am", "12:00am", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm"];
+$(document).ready(function() {
+    const span = $(".close");
+    const modal = $("#advModal");
+    const modalSel = $(".modalSel");
+    const open = $(".open");
+    const openR = $(".openRepo");
+    const next = $(".next");
+    const previous = $(".previous");
+    const modalTitle = $(".appTitle");
+    const modalTxt = $(".description");
+    const view = $("#view");
+   
+    //button to move past the hero section
+    view.on("click", function(){
+        location.href = "#about";
+    });
 
-//Function to check if the hour has changed --- not working
-function hourChecker() {
-    var currentHour = hour;
-    setInterval(function(){
-        hour = moment().format('HH');
-        if(currentHour !== hour) {
-            currentHour = hour;
-            location.reload();
+    //button to go from bottom to top
+    $("#backUp").on("click", function(){
+        location.href = "#home";
+    });
+
+    //display navigation bar after scrolling past hero section
+    let isFixed = false;
+    $(document).on("scroll", function(){
+        const navbar = $(".navi");
+        const hSHeight = $("section").height();
+
+        if($(window).scrollTop()>=hSHeight && !isFixed) {
+            isFixed = true
+            navbar.removeClass("hidden");
+            navbar.addClass("page-link");
         }
-    }, 10000)
-}
 
-hourChecker();
-
-//Function to check if the day has changed upon opening and clears local storage if it's a new day
-function dayChecker() {
-    if(localStorage.getItem("date")!==moment().format("DD")) {
-        localStorage.clear();
-    }
-}
-
-dayChecker();
-
-function dateDisplay(){
-    $(".time-date").text(moment().format('dddd Do MMMM')); 
-}
-
-dateDisplay();
-
-//Function to dynamically display the planner
-function listDisplay(){
-    for(i=0; i<time.length; i++) {
-        
-        var row = $("<div>");
-        var column = $("<div>");
-        var input = $("<textarea>");
-        var save = $("<div>");
-        var button = $("<button>")
-
-        //Create a row for each time period
-        row.attr("class", "row");
-        row.attr("style", "margin-bottom:10px;")
-
-        //Creating the space for the time display
-        column.attr("class", "col-sm-2 text-center");
-        column.attr("id", "time"+i);
-        
-        column.attr("style", "background-color:white;");
-        column.text(time[i]);
-
-        //Creating text area input
-        input.attr("class", "col-sm-8 inputarea");
-        input.attr("style", "width:100%; height:100%; resize:none;");
-        input.attr("id", "textarea"+i);
-
-        //Creating the area for the save button
-        save.attr("class", "col-sm-2 text-center save");
-        
-
-        //Creating the save button
-        button.text("Save");
-        button.attr("class", "save-button");
-        button.attr("id", i);
-        button.attr("style", "height:90%; width:100%;");
-
-        row.append(column);
-        row.append(input);
-        row.append(save);
-        save.append(button);
-        planner.append(row);
-    }
-
-    //Creating clear buttons
-    var row = $("<div>");
-    var clearAllB = $("<button>");
-    var clearB = $("<button>");
-
-    clearB.attr("class", "clear-button bg-primary");
-    clearB.attr("style", "margin-top:10px; margin-left:50%; transform:translate(-50%);")
-    clearB.text("Clear");
-
-    clearAllB.attr("class", "clearAll-button bg-danger");
-    clearAllB.attr("style", "margin-top:10px; margin-left:50%; transform:translate(-50%);")
-    clearAllB.text("Clear All");
-
-    //Appending the buttons
-    row.append(clearB);
-    row.append(clearAllB);
-    planner.append(row);
-}
-
-listDisplay();
-
-//Function to load any local storage saved data into the textareas
-function loadSaved(){
-    for(i=0; i<time.length; i++){
-        $("#textarea"+i).val(localStorage.getItem(time[i]));
-    }
-}
-
-loadSaved();
-
-//Variable to determine the current time/hour
-var currentTime = hour;
-
-//function to get the first number of the time array inputs and check if there is a second
-function timeCheck(i){
-    if(time[i].charAt(1)===":") {
-        //amPmChecker();
-        return parseInt(time[i].charAt(0));
-    }
-    else {
-        //amPmChecker();
-        return parseInt(time[i].charAt(0)+time[i].charAt(1));
-    }
-}
-
-//function to check AM/PM and return result in 24 hour time
-function amPmChecker(i){
-    if(time[i].charAt(time[i].length-2)==="p")
-        return timeCheck(i)+12;
-    else {
-        return timeCheck(i);
-    }
-}
-
-//Function to use the current time and add/subtract classes to textarea's to show whether the time has passed, current or future
-function currentDisplay(){
-
-    for(i=0; i<time.length; i++){
-        if(amPmChecker(i) < currentTime){
-            $("#textarea"+i).attr("class", "col-sm-8 inputarea past");
+        if($(window).scrollTop()<hSHeight && isFixed) {
+            isFixed = false;
+            navbar.slideUp(300, function(){
+                navbar.removeClass("page-link").show();
+                navbar.addClass("hidden");
+            });
         }
-        else if(amPmChecker(i) == currentTime){
-            $("#textarea"+i).attr("class", "col-sm-8 inputarea present");  
-        }
-        else if(amPmChecker(i) > currentTime){
-            $("#textarea"+i).attr("class", "col-sm-8 inputarea future"); 
-        }
+    });
+
+    //timeline settings
+    $(".single-item > span").on("click", function() {
+        $(".single-item").removeClass("single-item-active").fadeToggle(200);
+        $(this).parent().addClass("single-item-active").fadeToggle(200);
+        const pos = this.id;
+        displayInfo(pos);
+    });
+
+    const displayInfo = param1 => {
+        $(".information").empty();
+
+        const div1 = $("<div>").addClass("card");
+        const div2 = $("<div>").addClass("card-header");
+        const div3 = $("<div>").addClass("card-body");
+        const div4 = $("<div>").addClass("card-info");
+        const h5 = $("<h5>").addClass("card-title");
+        const img = $("<img>").addClass("timeline-picture img-fluid");
+        const p = $("<p>").addClass("card-text");
+
+        div2.text(info[param1].feature);
+        h5.text(info[param1].header);
+        p.html(info[param1].text);
+
+        div4.attr("style", "float:right;");
+        img.attr("src", info[param1].url);
+        img.attr("style","max-height: 200px;max-width: 200px, margin-left:10px;");
+
+        div4.append(img);
+        div3.append(h5, div4, p);
+        div1.append(div2,div3);
+        $(".information").append(div1);
     }
-}
 
-currentDisplay();
+    //modal settings for portfolio
+    let y = 0;
+    let x = 0;
 
-//Event listener on the save button to save any textarea value to the local storage
-$(".save").on("click", function(event){
-    var save = event.target.id;
-    var time = $("#time"+save).text();
-    var input = $("#textarea"+save).val();
-    var day = moment().format('DD');
-    localStorage.setItem("date", day);
-    localStorage.setItem(time, input);
-})
-
-//Button to clear all current textarea input and local storage 
-$(".clearAll-button").on("click", function(){
-    if(confirm("Warning this will clear all current inputs and local storage as well, do you wish to proceed?")) {
-        $(".inputarea").val("");
-        localStorage.clear();
+    const initial = () => {
+        modalSel.attr("src", images[x][y].url);
+        const h = $("<h3>").text(images[x][y].title);
+        const p = $("<p>").text(images[x][y].text);
+        modalTitle.empty();
+        modalTxt.empty();
+        modalTitle.append(h);
+        modalTxt.append(p);
     }
-})
+    initial();
+    
+    // on click functions to control the image slideshow 
+    next.on("click", () => {
+        if(y==images[x].length-1) {
+            y = 0;
+        }
+        else {
+            y = y+1;
+        }
+        initial();
+    });
 
-//Button to clear just the current inputs, upon refresh local storage values will still be inserted
-$(".clear-button").on("click", function(){
-    $(".inputarea").val("");
-})
+    previous.on("click", () => {
+        if(y==0) {
+            y = images[x].length-1;
+        }
+        else {
+            y = y-1;
+        }
+        initial()
+    });
+
+    open.on("click", () => window.open(images[x][y].href, "_blank"));
+    openR.on("click", () => window.open(images[x][y].repo, "_blank"));
+
+    //on click functions for changing the portfolio libraries
+    $(".types").on("click", function() {
+        y = 0;
+        x = $(this).attr("value");
+        initial(x);
+    });
+
+    //on click to open the advanced search modal
+    modalSel.on("click", () => {
+        modal.css("display", "block");
+        $(".modalPic").attr({
+            src: images[x][y].url,
+        });
+        $(".modalPic").attr("style", "max-width:300px; max-height:300px;"); 
+    });
+
+    $(document).click(function(event) {
+        //if you click on anything except the modal itself or the "open modal" link, close the modal
+        if (!$(event.target).closest(modal).length) {
+          $("body").find(".modal").removeClass("visible");
+        }
+    });
+
+    //clicking the x button will close the modal
+    span.on("click", () => modal.css("display", "none"));
+
+    //on click event for social media icons
+    $(".icons").on("click", function() {
+        window.open($(this).attr("value"), "blank");
+    });
+
+    //hover function for enter button
+    view.mouseenter(function() {
+        $("#arrow").text(" \u2193");
+    });
+
+    view.mouseleave(function() {
+        $("#arrow").text("\u2192");
+    });
+});
