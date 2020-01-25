872eaf876427f80106143a7213178a4f59951efb
EMC <edwardcoad.ec@gmail.com>
2020-01-25 16:21:51 +1030

    adding template generator

--- assets/js/information.js
+++ assets/js/information.js
@@ -35,11 +35,11 @@ const images = [ //information to do with the portfolio
             text: "A node.js application to dynamically create an engineering team based on user inputs. A team would consist of one manager and any number of engineers or interns. Each team member will have thier name, Idenitificaion number and email address recorded along with a unqiue piece of information for each relating role, i.e Team manager - office number, Engineer - github account and Intern - school. The user will be prompted in the order of manager, engineers and then interns. At the end of each engineer or intern input the user will be asked if they want to add another member of that role, selecting no will move to the next role or finish the input depending on where they are at. Each of the inputs are validated upon entry and will not allow incorrect answers, i.e numbers in names, email format etc. If an incorrect answer is given the user is prompted to input a valid one. After the user has enter all the relavent information for each team member the application will produce a html file displaying each team member in thier own personal info card."
         },
         {
-            title: "",
-            url: "assets/images/underConst.png", 
-            href: "",
-            repo: "",
-            text: ""
+            title: "Template Generator",
+            url: "assets/images/templateEngine.jpg", 
+            href: "https://github.com/EdwardEMC/templateEngine",
+            repo: "https://github.com/EdwardEMC/templateEngine",
+            text: "A node.js application to dynamically create an engineering team based on user inputs. A team would consist of one manager and any number of engineers or interns. Each team member will have their name, Idenitificaion number and email address recorded along with a unqiue piece of information for each relating role, i.e Team manager - office number, Engineer - github account and Intern - school."
         },
         {
             title: "",
